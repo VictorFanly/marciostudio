d59ca00e4f73d0e8c75c53c2a921c14ee7944eed
Victor Fanly <victor.lxxxla@gmail.com>
2022-10-24 15:45:36 -0300

    Commit dia 3 de Desenvolvimento

---
+++ Site/js/script.js
@@ -0,0 +1,21 @@
+function mostrar() {
+    for(var i=1; i<6; i++){
+        document.getElementById("t"+i).hidden=true;
+    }
+    var obj=event.target.dataset.nome;
+    document.getElementById(obj).hidden=false;
+}
+
+function inicia(){
+    for(var i=2; i<6; i++){
+        document.getElementById("t"+i).hidden=true;
+    }
+    document.getElementById("PicMarcio").addEventListener("click",mostrar);
+    document.getElementById("PicCorreia").addEventListener("click",mostrar);
+    document.getElementById("PicMarine").addEventListener("click",mostrar);
+    document.getElementById("PicKim").addEventListener("click",mostrar);
+    document.getElementById("PicQueise").addEventListener("click",mostrar);
+
+}
+
+window.addEventListener("load",inicia);
