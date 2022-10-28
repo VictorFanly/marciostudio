66ebcfe994e2bc020220a75170a68119b9151fc9
Victor Fanly <victor.lxxxla@gmail.com>
2022-10-28 16:37:40 -0300

    Commit Dia 7 de Desenvolvimento

--- Site/js/estetica.js
+++ Site/js/estetica.js
@@ -1,16 +1,15 @@
-const productContainers = [...document.querySelectorAll('.estetica-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+var swiper = new Swiper(".estetica-container", {
+    slidesPerView: 2.333333333,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
