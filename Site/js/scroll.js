21cdbed6a70416e99b704588c28028b1a089e2d3
Victor Fanly <victor.lxxxla@gmail.com>
2022-11-07 17:03:42 -0300

    Commit Posicionamento

---
+++ Site/js/scroll.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.logo', { duration: 2000 }); 
+
+sr.reveal('.equipe-container', { duration: 1000 });
+
+sr.reveal('.estudio-container', {
+    duration: 5000,
+    opacity: 1
+}); 
+
+
+sr.reveal('.portfolio-container', { duration: 1000 }); 
+
+sr.reveal('.estetica-container', { duration: 1000 }); 
+
+sr.reveal('.contato-container', { duration: 1000 }); 
